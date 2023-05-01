16857df9b50ae1611b1599224076a16dcba422c1
diou <diou@dioudeMacBook-Pro.local>
2023-05-01 13:25:30 +0800

    developing...

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Drawing, { DrawingProps, useDrawing } from "react-drawing-library"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import DrawingProps from 'react-drawing-library/dist/esm/'
+// import { Stage } from "react-konva"
+
+const shapeDraw: DrawingProps["draw"] = {
+  type: "Rectangle",
+  color: "#52acff",
+  text: "aa",
 }
 
-export default App;
+function App() {
+  const imageUrl =
+    "https://tse1-mm.cn.bing.net/th/id/OIP-C.rQ9MEBHEGcjQ8Yc2YB23YQHaEj?w=315&h=193&c=7&r=0&o=5&dpr=2&pid=1.7"
+
+  const shapes = useDrawing()
+
+  const [draw, setDraw] = useState<DrawingProps["draw"] | undefined>(shapeDraw)
+
+  return (
+    <div>
+      <button onClick={() => setDraw(undefined)}>clear draw</button>
+      <button onClick={() => setDraw(shapeDraw)}>rectangle</button>
+      <button onClick={() => console.log(shapes.shapes)}>console shapes</button>
+      <Drawing
+        draw={draw}
+        shapes={shapes}
+        imageUrl={imageUrl}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
+    </div>
+  )
+}
+
+export default App
